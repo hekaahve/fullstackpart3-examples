8acd9963a4b597fd8afac69ba2173c74bd1a02b3
hekaahve <heini.k.ahven@student.jyu.fi>
2021-10-21 17:43:35 +0300

    mongo to own module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -11,49 +13,26 @@ app.use(express.static('build'))
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2020-01-10T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2020-01-10T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2020-01-10T19:20:14.298Z",
-      important: true
-    }
-  ]
   app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
   
   app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+      res.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
-  
+    
     response.status(204).end()
   })
   
@@ -66,22 +45,20 @@ let notes = [
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
+    if (body.content === undefined) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+    })
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   morgan(function (tokens, req, res) {
@@ -93,9 +70,9 @@ let notes = [
       tokens['response-time'](req, res), 'ms'
     ].join(' '),
     tokens.res.body(req, res, 'body')
-  })//test
+  })
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
