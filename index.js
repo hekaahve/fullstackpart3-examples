b70d7d8d9de6059eefde269c5184878cb5b7facd
hekaahve <heini.k.ahven@student.jyu.fi>
2022-01-02 13:57:42 +0200

    errorhandling added and PUT request

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const Note = require('./models/note')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
   app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -23,17 +40,42 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :b
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
     })
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    
-    response.status(204).end()
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
   
   const generateId = () => {
@@ -42,35 +84,30 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :b
       : 0
     return maxId + 1
   }
-  app.post('/api/notes', (request, response) => {
+
+  app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
   
-    if (body.content === undefined) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    const note = {
+      content: body.content,
+      important: body.important,
     }
   
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    })
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
-
-  morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' '),
-    tokens.res.body(req, res, 'body')
-  })
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
+  
+  // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
