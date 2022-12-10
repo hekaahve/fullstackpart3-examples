f07ce2e1199acf05019a0fcdf684db4a1cb8bfc7
Heini Ahven <heini.ahven@gmail.com>
2022-12-10 13:51:03 +0200

    added deletemany and insertMany in beforeEach

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,21 +2,16 @@ const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
 notesRouter.get("/:id", async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
-    }
-  } catch (exception) {
-    next(exception);
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
 });
 
@@ -28,26 +23,17 @@ notesRouter.post("/", async (request, response, next) => {
     important: body.important || false,
     date: new Date(),
   });
-  try {
-    const savedNote = await note.save();
+  const savedNote = await note.save();
 
-    response.status(201).json(savedNote);
-  } catch (exception) {
-    next(exception);
-  }
+  response.status(201).json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id);
-
-    response.status(204).end();
-  } catch (exception) {
-    next(exception);
-  }
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", async (request, response, next) => {
   const body = request.body;
 
   const note = {
@@ -55,11 +41,10 @@ notesRouter.put("/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+  });
+  response.json(updatedNote);
 });
 
 module.exports = notesRouter;
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,12 +8,7 @@ const Note = require("../models/note");
 
 beforeEach(async () => {
   await Note.deleteMany({});
-
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  await Note.insertMany(helper.initialNotes);
 });
 
 test("notes are returned as json", async () => {
