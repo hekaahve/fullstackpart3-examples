3e62e1b0a362d66287741d52e40bde0c7fa9037f
Heini Ahven <heini.ahven@gmail.com>
2023-01-13 20:58:23 +0200

    middlewares for token added

--- controllers/login.js
+++ controllers/login.js
@@ -21,7 +21,10 @@ loginRouter.post("/", async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  //token expires in 60*60 seconds / in one hour
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  });
 
   response
     .status(200)
